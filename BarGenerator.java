4074c663a3095754d31b3e0f641c7245b64e5af8
FoyerD <amitfoye@post.bgu.ac.il>
2023-05-30 12:48:20 +0300

    added bar generator class

---
+++ BarGenerator.java
@@ -0,0 +1,30 @@
+public class BarGenerator {
+    enum Color {
+        //Color end string, color reset
+        RESET("\033[0m"),
+
+        // Regular Colors. Normal color, no bold, background color etc.
+        BLACK("\033[0;30m"),    // BLACK
+        RED("\033[0;31m"),      // RED
+        GREEN("\033[0;32m"),    // GREEN
+        YELLOW("\033[0;33m"),   // YELLOW
+        BLUE("\033[0;34m"),     // BLUE
+
+        // Background
+        RED_BACKGROUND("\033[41m"),     // RED
+        GREEN_BACKGROUND("\033[42m"),   // GREEN
+        YELLOW_BACKGROUND("\033[43m"),  // YELLOW
+        BLUE_BACKGROUND("\033[44m");  // BLUE
+
+        private final String code;
+
+        Color(String code) {
+            this.code = code;
+        }
+
+        @Override
+        public String toString() {
+            return code;
+        }
+    }
+}
