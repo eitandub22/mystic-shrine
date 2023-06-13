e414aff6477a29c373c2e1a90a11ead87add21c8
eitandub22 <eitandu@post.bgu.ac.il>
2023-06-13 12:29:28 +0300

    changed compareTo in TilePosComperator

--- BusinessLayer/TilePosComperator.java
+++ BusinessLayer/TilePosComperator.java
@@ -5,10 +5,6 @@ import java.util.Comparator;
 public class TilePosComperator implements Comparator<Tile> {
     public int compare(Tile t1, Tile t2)
     {
-        if(t1.getPosition().getY() - t2.getPosition().getY() != 0)
-        {
-            return t1.getPosition().getY() - t2.getPosition().getY();
-        }
-        return t1.getPosition().getX() - t2.getPosition().getX();
+        return t1.getPosition().compareTo(t2.getPosition());
     }
 }
